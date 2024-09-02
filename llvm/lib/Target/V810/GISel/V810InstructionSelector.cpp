12aafaed879ba5fafc5baa2d1799078d54add381
Simon Gellis <simongellis@gmail.com>
2024-09-02 00:15:12 -0400

    Translate some patterns to gisel

--- llvm/lib/Target/V810/GISel/V810InstructionSelector.cpp
+++ llvm/lib/Target/V810/GISel/V810InstructionSelector.cpp
@@ -1,6 +1,7 @@
 #include "V810InstructionSelector.h"
 #include "llvm/CodeGen/GlobalISel/GIMatchTableExecutorImpl.h"
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
+#include "llvm/CodeGen/GlobalISel/MIPatternMatch.h"
 
 using namespace llvm;
 
@@ -19,6 +20,8 @@ public:
                           V810RegisterBankInfo &RBI);
   bool select(MachineInstr &MI) override;
   static const char *getName() { return DEBUG_TYPE; }
+
+  ComplexRendererFns selectAddrRI(MachineOperand &Root) const;
 private:
   const V810Subtarget &STI;
   const V810InstrInfo &TII;
@@ -59,6 +62,68 @@ V810InstructionSelector::V810InstructionSelector(const V810TargetMachine &TM,
 
 bool V810InstructionSelector::select(MachineInstr &MI) {
   return selectImpl(MI, *CoverageInfo);
+}
+
+InstructionSelector::ComplexRendererFns
+V810InstructionSelector::selectAddrRI(MachineOperand &Root) const {
+  using namespace llvm::MIPatternMatch;
+
+  MachineFunction &MF = *Root.getParent()->getParent()->getParent();
+  MachineRegisterInfo &MRI = MF.getRegInfo();
+
+  if (!Root.isReg())
+    return std::nullopt;
+  
+  MachineInstr *RootI = MRI.getVRegDef(Root.getReg());
+
+  if (RootI->getOpcode() == TargetOpcode::G_FRAME_INDEX) {
+    return {{
+      [=](MachineInstrBuilder &MIB) { MIB.add(RootI->getOperand(1)); },
+      [=](MachineInstrBuilder &MIB) { MIB.addImm(0); },
+    }};
+  }
+
+  Register BaseReg;
+  APInt Offset;
+  if (mi_match(RootI, MRI, m_GPtrAdd(m_Reg(BaseReg), m_ICst(Offset)))
+      && Offset.isSignedIntN(16)) {
+    MachineInstr *LHS = MRI.getVRegDef(BaseReg);
+    if (LHS->getOpcode() == TargetOpcode::G_FRAME_INDEX) {
+      return {{
+        [=](MachineInstrBuilder &MIB) { MIB.add(LHS->getOperand(1)); },
+        [=](MachineInstrBuilder &MIB) { MIB.addImm(Offset.getSExtValue()); }
+      }};
+    }
+
+    return {{
+      [=](MachineInstrBuilder &MIB) { MIB.addReg(BaseReg); },
+      [=](MachineInstrBuilder &MIB) { MIB.addImm(Offset.getSExtValue()); }
+    }};
+  }
+
+  APInt ConstAddress;
+  if (mi_match(Root.getReg(), MRI, m_ICst(ConstAddress))) {
+    assert(ConstAddress.isSignedIntN(32));
+    // When working with constant addresses, MOVHI the high bits and stick the low bits in the offset
+    uint64_t lo = EvalLo(ConstAddress.getSExtValue());
+    uint64_t hi = EvalHi(ConstAddress.getSExtValue());
+    if (hi == 0) {
+      return {{
+        [=](MachineInstrBuilder &MIB) { MIB.addReg(V810::R0); },
+        [=](MachineInstrBuilder &MIB) { MIB.addImm(lo); }
+      }};
+    }
+    Register HiReg = MRI.createVirtualRegister(&V810::GenRegsRegClass);
+    return {{
+      [=](MachineInstrBuilder &MIB) {
+        MachineIRBuilder(*MIB.getInstr()).buildInstr(V810::MOVHI, {HiReg}, {hi});
+        MIB.addReg(HiReg);
+      },
+      [=](MachineInstrBuilder &MIB) { MIB.addImm(lo); }
+    }};
+  }
+
+  return std::nullopt;
 }
 
 InstructionSelector *llvm::createV810InstructionSelector(

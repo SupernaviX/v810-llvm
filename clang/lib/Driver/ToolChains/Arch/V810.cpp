cfd5f3183aadbbf8c7e5c922c0875bcdb307127f
Simon Gellis <simongellis@gmail.com>
2025-11-27 13:32:46 -0500

    Fix

--- clang/lib/Driver/ToolChains/Arch/V810.cpp
+++ clang/lib/Driver/ToolChains/Arch/V810.cpp
@@ -1,7 +1,7 @@
 #include "V810.h"
 #include "clang/Driver/Driver.h"
 #include "clang/Driver/DriverDiagnostic.h"
-#include "clang/Driver/Options.h"
+#include "clang/Options/Options.h"
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/Option/ArgList.h"
 #include "llvm/TargetParser/Host.h"
@@ -14,13 +14,13 @@ using namespace llvm::opt;
 std::string v810::getV810TargetCPU(const Driver &D, const ArgList &Args,
                                    const llvm::Triple &Triple) {
 
-  if (const Arg *A = Args.getLastArg(clang::driver::options::OPT_march_EQ)) {
+  if (const Arg *A = Args.getLastArg(clang::options::OPT_march_EQ)) {
     D.Diag(diag::err_drv_unsupported_opt_for_target)
         << A->getSpelling() << Triple.getTriple();
     return "";
   }
 
-  if (const Arg *A = Args.getLastArg(clang::driver::options::OPT_mcpu_EQ)) {
+  if (const Arg *A = Args.getLastArg(clang::options::OPT_mcpu_EQ)) {
     StringRef CPUName = A->getValue();
     return std::string(CPUName);
   }

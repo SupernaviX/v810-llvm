6ba39c7b77ebd12e6c718962a255ffb2b6670847
Simon Gellis <simongellis@gmail.com>
2026-02-01 13:30:04 -0500

    Fix comments on their own line

--- llvm/test/MC/V810/comments.s
+++ llvm/test/MC/V810/comments.s
@@ -1,4 +1,7 @@
 # RUN: llvm-mc %s -arch=v810 -filetype=obj | llvm-objdump -dr - | FileCheck %s
 
+    /* this one on its own initial line */
+    /* also, this one */
     # CHECK: st.h r0, 0x0[r6]
     st.h r0, /* ignore this comment */ [r6] /* and this one */
+    /* and this one on its own line */

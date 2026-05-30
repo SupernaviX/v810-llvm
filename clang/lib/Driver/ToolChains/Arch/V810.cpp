01b7ff1ca8ffc87296f7e9dd3b1b157c5252b8a9
Simon Gellis <simongellis@gmail.com>
2026-05-30 15:59:47 -0400

    Delete crash-on-mac-only test

--- clang/lib/Driver/ToolChains/Arch/V810.cpp
+++ clang/lib/Driver/ToolChains/Arch/V810.cpp
@@ -14,12 +14,6 @@ using namespace llvm::opt;
 std::string v810::getV810TargetCPU(const Driver &D, const ArgList &Args,
                                    const llvm::Triple &Triple) {
 
-  if (const Arg *A = Args.getLastArg(clang::options::OPT_march_EQ)) {
-    D.Diag(diag::err_drv_unsupported_opt_for_target)
-        << A->getSpelling() << Triple.getTriple();
-    return "";
-  }
-
   if (const Arg *A = Args.getLastArg(clang::options::OPT_mcpu_EQ)) {
     StringRef CPUName = A->getValue();
     return std::string(CPUName);
